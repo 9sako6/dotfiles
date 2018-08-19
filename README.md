fe3750cd9405724d6920f5333f1f5080f6f4e0ba
9sako6 <31821663+9sako6@users.noreply.github.com>
2018-08-19 18:17:51 +0900

    add bash

---
+++ README.md
@@ -0,0 +1,12 @@
+# dotfiles
+
+zsh, bash
+
+## Set Up
+
+At `$HOME`:
+
+```shell
+$ git clone https://github.com/9sako6/dotfiles.git
+$ ./setup.sh
+```
--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env zsh
 
+# zsh
 ln -sf ~/dotfiles/.zsh ~/
 ln -sf ~/dotfiles/.zshenv ~/.zshenv
+
+# bash
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
