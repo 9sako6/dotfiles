3628a6bea7073df3a51cb5eca4aa4be5abf5f6af
9sako6 <31821663+9sako6@users.noreply.github.com>
2018-08-22 15:26:07 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env zsh
 
+if ! $git_exists || ! $vim_exists || ! $zsh_exists; then
+  echo -e "\033[0;31mError: git or vim or zsh is not installed.\033[0m"
+  echo -e "\033[0;31mPlease install them\033[0m"
+  exit
+fi
+
 # zsh
-mkdir ~/.zsh
+if [ ! -d ~/.zsh ]; then
+  mkdir ~/.zsh
+fi
 ln -sf ~/dotfiles/.zsh/.zshrc ~/.zsh/.zshrc
 ln -sf ~/dotfiles/.zsh/.zshenv ~/.zshenv
 
@@ -11,3 +19,5 @@ ln -sf ~/dotfiles/.bash/.bashrc ~/.bashrc
 
 # vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+
+echo -e "\033[0;32mdotfiles are successfully installed!\033[0m"
