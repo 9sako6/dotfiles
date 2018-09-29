655fc27a0fa95af4ad1f8ca6f109b98d0cb2f3f8
9sako6 <31821663+9sako6@users.noreply.github.com>
2018-09-29 15:01:45 +0900

    fix clean.sh

--- etc/clean.sh
+++ etc/clean.sh
@@ -4,10 +4,8 @@ DOTFILES=( .bash_profile .bashrc .vimrc .zshenv .zsh mybin )
 for f in ${DOTFILES[@]}
 do
   if [ -h $HOME/$f ]; then
-    # ln -sv $HOME/dotfiles/$f $HOME/
     rm -rv $HOME/$f
   else
-    # ln -sv $HOME/dotfiles/$f $HOME/$f
     echo "${f} is not symbolic link"
   fi
 done
