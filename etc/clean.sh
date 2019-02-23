28235218772b8c99cf141c8c7ccd7e80da0431a7
9sako6 <31821663+9sako6@users.noreply.github.com>
2019-02-23 22:01:38 +0900

    .

--- etc/clean.sh
+++ etc/clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DOTFILES=( .bash_profile .bashrc .vimrc .zshenv .zshrc .zshenv.zwc .zshrc.zwc mybin .commit_template .tmux.conf )
+DOTFILES=( .bash_profile .bashrc .vimrc .zshenv .zshrc .zshenv.zwc .zshrc.zwc mybin .commit_template .tmux.conf .xonshrc)
 
 for f in ${DOTFILES[@]}
 do
