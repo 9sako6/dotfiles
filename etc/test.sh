d142d385aa94ddf403fba1657cd5e1495d4a90e3
9sako6 <yutaro.taira@esm.co.jp>
2021-05-30 11:21:52 +0900

    Refactoring deploy scripts

--- etc/test.sh
+++ etc/test.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 # set -e
 
-function command_exists() {
-  command=${1}
-
-  if type ${command}; then
-    :
-  else
-    exit 1
-  fi
-}
-
 make help
 make init
 make clean
 make deploy
-
-# NOTE: fail in github actions
-# command_exists fzf
--- etc/utils.sh
+++ etc/utils.sh
@@ -1,9 +1,21 @@
 function print_error() {
   echo -e "\033[0;31mError: ${1}\033[0m"
 }
+
 function print_success() {
   echo -e "\033[0;32mSuccess: ${1}\033[0m"
 }
+
 function print_info() {
   echo -e "\033[0;34mInfo: ${1}\033[0m"
+}
+
+function command_exists() {
+  command=${1}
+
+  if type ${command}; then
+    :
+  else
+    exit 1
+  fi
 }
