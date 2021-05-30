d040bc87ed68b82450ae6c48bbae5bab67cdccd3
9sako6 <yutaro.taira@esm.co.jp>
2021-05-30 20:13:58 +0900

    Use tput command to set font color
    環境による色の差をラップしてくれる

--- etc/utils.sh
+++ etc/utils.sh
@@ -1,13 +1,13 @@
 function print_error() {
-  echo -e "\033[0;31mError: ${1}\033[0m"
+  tput setaf 1 && echo "Error: ${1}"
 }
 
 function print_success() {
-  echo -e "\033[0;32mSuccess: ${1}\033[0m"
+  tput setaf 2 && echo "Success: ${1}"
 }
 
 function print_info() {
-  echo -e "\033[0;34mInfo: ${1}\033[0m"
+  tput setaf 4 && echo "Info: ${1}"
 }
 
 function command_exists() {
