711f5740ffddc30750b6eff0e5713c0b6abdbaab
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-11-19 12:41:21 +0900

    debug

--- install.sh
+++ install.sh
@@ -17,5 +17,5 @@ cd "${HOME}/dotfiles"
 export DENO_INSTALL="${HOME}/.local"
 export PATH="${DENO_INSTALL}/bin:${PATH}"
 curl -fsSL https://deno.land/x/install/install.sh | sh
-
+deno -h
 deno run --allow-write --allow-read --allow-env main.ts
