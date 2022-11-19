be594027c656661e6a8363a77d134984cf713641
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-11-19 12:05:29 +0900

    Fix deno path env

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # Install Deno
-curl -fsSL https://deno.land/x/install/install.sh | sh
 export DENO_INSTALL="$HOME/.local"
 export PATH="$DENO_INSTALL/bin:$PATH"
+curl -fsSL https://deno.land/x/install/install.sh | sh
 
 deno run --allow-write --allow-read --allow-env main.ts
