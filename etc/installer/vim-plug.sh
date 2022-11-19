e99bb568467184f80d0ca8bf066e769e63f94ea6
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-11-19 21:57:19 +0900

    Delete unused files

--- etc/installer/vim-plug.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# set -e
-
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-echo 'Please run `:PlugInstall` in vim'
