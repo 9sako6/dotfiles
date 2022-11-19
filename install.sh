a775216f353789e5cf570b64a92b54f457405b37
9sako6 <31821663+9sako6@users.noreply.github.com>
2022-11-19 12:11:42 +0900

    Clone dotfiles

--- install.sh
+++ install.sh
@@ -1,6 +1,21 @@
+if [ ! -d "${HOME}/dotfiles" ]; then
+  echo "Start to clone dotfiles repository"
+  cd "${HOME}"
+  if [ ${GITHUB_REF} ]; then
+    # in GitHub Actions
+    branch_name=$(echo "${GITHUB_REF##*/}")
+    git clone -b ${branch_name} https://github.com/9sako6/dotfiles
+  else
+    git clone https://github.com/9sako6/dotfiles
+  fi
+  echo "Finish to clone dotfiles repository"
+fi
+
+cd "${HOME}/dotfiles"
+
 # Install Deno
-export DENO_INSTALL="$HOME/.local"
-export PATH="$DENO_INSTALL/bin:$PATH"
+export DENO_INSTALL="${HOME/}.local"
+export PATH="${DENO_INSTALL}/bin:${PATH}"
 curl -fsSL https://deno.land/x/install/install.sh | sh
 
 deno run --allow-write --allow-read --allow-env main.ts
