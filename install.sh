c41682120524dd1485029710a83d22d3aa95fd39
9sako6 <31821663+9sako6@users.noreply.github.com>
2023-05-07 02:07:59 +0900

    Fix tests

--- install.sh
+++ install.sh
@@ -11,16 +11,6 @@ if [ ! -d "${HOME}/dotfiles" ]; then
   echo "Finish to clone dotfiles repository"
 fi
 
-# Install aqua
-curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.1.1/aqua-installer
-echo "c2af02bdd15da6794f9c98db40332c804224930212f553a805425441f8331665  aqua-installer" | sha256sum -c
-chmod +x aqua-installer
-./aqua-installer
-source ~/.zshenv
-source ~/.zshrc
-
-aqua --version
-
 cd "${HOME}/dotfiles"
 
 # Install Deno
@@ -29,6 +19,17 @@ export PATH="${DENO_INSTALL}/bin:${PATH}"
 curl -fsSL https://deno.land/x/install/install.sh | sh
 
 deno run --allow-write --allow-read --allow-env main.ts
+
+source ~/.zshenv
+source ~/.zshrc
+
+# Install aqua
+curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.1.1/aqua-installer
+echo "c2af02bdd15da6794f9c98db40332c804224930212f553a805425441f8331665  aqua-installer" | sha256sum -c
+chmod +x aqua-installer
+./aqua-installer
+
+aqua --version
 
 # Install deps with aqua
 aqua i -a
