8dc3c281ad2a309e58e3aec37b43b8b7cb0ece2c
9sako6 <31821663+9sako6@users.noreply.github.com>
2023-05-07 02:24:31 +0900

    Drop tests when deps are not installed

--- install.sh
+++ install.sh
@@ -35,5 +35,16 @@ aqua --version
 aqua i -a
 
 # Test installed deps
-fzf --version
-deno --version
+if ! command -v fzf > /dev/null; then
+  echo "Error: fail to install fzf." >&2
+  exit 1
+else
+  fzf --version
+fi
+
+if ! command -v deno > /dev/null; then
+  echo "Error: fail to install deno." >&2
+  exit 1
+else
+  deno --version
+fi
