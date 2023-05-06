39c895581d92819600ae6709ecc3495798837e30
9sako6 <31821663+9sako6@users.noreply.github.com>
2023-05-06 15:39:11 +0900

    Display current context on prompt

--- dist/.zsh.local/prompt.zsh
+++ dist/.zsh.local/prompt.zsh
@@ -1,7 +1,13 @@
+# Show k8s current context
+zinit light jonmosco/kube-ps1
+KUBE_PS1_CTX_COLOR='magenta'
+KUBE_PS1_NS_COLOR='white'
+KUBE_PS1_PREFIX=''
+KUBE_PS1_SUFFIX=''
+KUBE_PS1_SEPARATOR=' '
+
 autoload -Uz colors
 colors
-PROMPT="%F{cyan}%n@%d
-( ;ᴗ;)っ%f "
 autoload -Uz vcs_info
 setopt prompt_subst
 zstyle ':vcs_info:git:*' check-for-changes true
@@ -10,4 +16,6 @@ zstyle ':vcs_info:git:*' unstagedstr "%F{red}+"
 zstyle ':vcs_info:*' formats "%F{green}%c%u[%b]%f"
 zstyle ':vcs_info:*' actionformats '[%b|%a]'
 precmd () { vcs_info }
-RPROMPT=$RPROMPT'${vcs_info_msg_0_}'
+RPROMPT=$RPROMPT'%F{cyan}。(｡>﹏<｡)'
+PROMPT="%F{cyan}%n@%d \${vcs_info_msg_0_} \$(kube_ps1)
+%F{cyan}( ;ᴗ;)っ%f "
