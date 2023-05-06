071dad68df39b68c30be9738451b522c7d3f1381
9sako6 <31821663+9sako6@users.noreply.github.com>
2023-05-07 02:28:07 +0900

    Check installed deps

--- install.sh
+++ install.sh
@@ -33,18 +33,3 @@ aqua --version
 
 # Install deps with aqua
 aqua i -a
-
-# Test installed deps
-if ! command -v fzf > /dev/null; then
-  echo "Error: fail to install fzf." >&2
-  exit 1
-else
-  fzf --version
-fi
-
-if ! command -v deno > /dev/null; then
-  echo "Error: fail to install deno." >&2
-  exit 1
-else
-  deno --version
-fi
---
+++ test.sh
@@ -0,0 +1,14 @@
+# Test installed deps
+if ! command -v fzf > /dev/null; then
+  echo "Error: fail to install fzf." >&2
+  exit 1
+else
+  fzf --version
+fi
+
+if ! command -v deno > /dev/null; then
+  echo "Error: fail to install deno." >&2
+  exit 1
+else
+  deno --version
+fi
