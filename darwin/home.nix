a273e38b07a1af435c5106403bbbfe317e8e68ef
Yutaro Taira <31821663+9sako6@users.noreply.github.com>
2026-08-08 09:58:41 +0900

    feat: Home Managerをnix-darwinへ統合

---
+++ darwin/home.nix
@@ -0,0 +1,39 @@
+{ config, dotfilesDirectory, ... }:
+
+let
+  homeRoot = "${dotfilesDirectory}/home";
+  outOfStore = relativePath:
+    config.lib.file.mkOutOfStoreSymlink "${homeRoot}/${relativePath}";
+  liveLink = relativePath: {
+    source = outOfStore relativePath;
+    force = true;
+  };
+in
+{
+  home.stateVersion = "26.05";
+
+  home.file = {
+    ".config" = liveLink ".config";
+    ".gitconfig" = liveLink ".gitconfig";
+    ".gitignore_global" = liveLink ".gitignore_global";
+    ".zsh.d" = liveLink ".zsh.d";
+    ".zshenv" = liveLink ".zshenv";
+    ".zshrc" = liveLink ".zshrc";
+    "apm.lock.yaml" = liveLink "apm.lock.yaml";
+    "apm.yml" = liveLink "apm.yml";
+    "mybin" = liveLink "mybin";
+
+    ".agents" = {
+      source = outOfStore ".agents";
+      recursive = true;
+    };
+    ".claude" = {
+      source = outOfStore ".claude";
+      recursive = true;
+    };
+    ".codex" = {
+      source = outOfStore ".codex";
+      recursive = true;
+    };
+  };
+}
