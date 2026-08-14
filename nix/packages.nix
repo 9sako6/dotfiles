c0fab12462f6190372e8d008d56765b1144ca986
Yutaro Taira <31821663+9sako6@users.noreply.github.com>
2026-08-15 00:33:59 +0900

    nix: share pinned user tool set

--- nix/packages.nix
+++ nix/packages.nix
@@ -1,41 +1,41 @@
-{ pkgs, ... }:
+{ pkgs }:
 
 let
+  bunPackage = pkgs.bun;
+  expectedBunVersion = "1.3.13";
+
   gitPackage = pkgs.git;
   expectedGitVersion = "2.55.0";
+
+  goPackage = pkgs.go_1_26;
+  expectedGoVersion = "1.26.5";
 
   quintPackage = pkgs.quint;
   expectedQuintVersion = "0.32.0";
 
   rustToolchain = pkgs.rustPackages_1_97;
   expectedRustVersion = "1.97.1";
-
-  goPackage = pkgs.go_1_26;
-  expectedGoVersion = "1.26.5";
 in
+assert pkgs.lib.assertMsg (bunPackage.version == expectedBunVersion)
+  "Bun version drifted: expected ${expectedBunVersion}, got ${bunPackage.version}";
+assert pkgs.lib.assertMsg (gitPackage.version == expectedGitVersion)
+  "Git version drifted: expected ${expectedGitVersion}, got ${gitPackage.version}";
+assert pkgs.lib.assertMsg (goPackage.version == expectedGoVersion)
+  "Go version drifted: expected ${expectedGoVersion}, got ${goPackage.version}";
+assert pkgs.lib.assertMsg (quintPackage.version == expectedQuintVersion)
+  "Quint version drifted: expected ${expectedQuintVersion}, got ${quintPackage.version}";
+assert pkgs.lib.assertMsg (rustToolchain.rustc.version == expectedRustVersion)
+  "Rust version drifted: expected ${expectedRustVersion}, got ${rustToolchain.rustc.version}";
 {
-  assertions = [
-    {
-      assertion = gitPackage.version == expectedGitVersion;
-      message = "Git version drifted: expected ${expectedGitVersion}, got ${gitPackage.version}";
-    }
-    {
-      assertion = quintPackage.version == expectedQuintVersion;
-      message = "Quint version drifted: expected ${expectedQuintVersion}, got ${quintPackage.version}";
-    }
-    {
-      assertion = rustToolchain.rustc.version == expectedRustVersion;
-      message = "Rust version drifted: expected ${expectedRustVersion}, got ${rustToolchain.rustc.version}";
-    }
-    {
-      assertion = goPackage.version == expectedGoVersion;
-      message = "Go version drifted: expected ${expectedGoVersion}, got ${goPackage.version}";
-    }
-  ];
+  packages = [
+    # Bun 1.3.13
+    bunPackage
 
-  home.packages = [
     # Git 2.55.0
     gitPackage
+
+    # Go 1.26.5
+    goPackage
 
     # Quint 0.32.0
     quintPackage
@@ -45,8 +45,5 @@ in
     rustToolchain.cargo
     rustToolchain.rustfmt
     rustToolchain.clippy
-
-    # Go 1.26.5
-    goPackage
   ];
 }
