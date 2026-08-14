67354f17154b418b3a9585157a3c71562adf107f
Yutaro Taira <31821663+9sako6@users.noreply.github.com>
2026-08-14 23:37:35 +0900

    home-manager: order user packages consistently

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -4,20 +4,24 @@ let
   gitPackage = pkgs.git;
   expectedGitVersion = "2.55.0";
 
+  goPackage = pkgs.go_1_26;
+  expectedGoVersion = "1.26.5";
+
   quintPackage = pkgs.quint;
   expectedQuintVersion = "0.32.0";
 
   rustToolchain = pkgs.rustPackages_1_97;
   expectedRustVersion = "1.97.1";
-
-  goPackage = pkgs.go_1_26;
-  expectedGoVersion = "1.26.5";
 in
 {
   assertions = [
     {
       assertion = gitPackage.version == expectedGitVersion;
       message = "Git version drifted: expected ${expectedGitVersion}, got ${gitPackage.version}";
+    }
+    {
+      assertion = goPackage.version == expectedGoVersion;
+      message = "Go version drifted: expected ${expectedGoVersion}, got ${goPackage.version}";
     }
     {
       assertion = quintPackage.version == expectedQuintVersion;
@@ -27,15 +31,14 @@ in
       assertion = rustToolchain.rustc.version == expectedRustVersion;
       message = "Rust version drifted: expected ${expectedRustVersion}, got ${rustToolchain.rustc.version}";
     }
-    {
-      assertion = goPackage.version == expectedGoVersion;
-      message = "Go version drifted: expected ${expectedGoVersion}, got ${goPackage.version}";
-    }
   ];
 
   home.packages = [
     # Git 2.55.0
     gitPackage
+
+    # Go 1.26.5
+    goPackage
 
     # Quint 0.32.0
     quintPackage
@@ -45,8 +48,5 @@ in
     rustToolchain.cargo
     rustToolchain.rustfmt
     rustToolchain.clippy
-
-    # Go 1.26.5
-    goPackage
   ];
 }
